ac9469adbb68b98ea088d2d043ae103aa5ad0b10
Vaisakh P V <vaisakhpv033@gmail.com>
2025-05-06 19:37:37 +0530

    Initial setup

---
+++ src/constants/index.js
@@ -0,0 +1,189 @@
+const navItems = [
+    {
+      name: "Home",
+      href: "#home",
+    },
+    {
+      name: "About",
+      href: "#about",
+    },
+    {
+      name: "Projects",
+      href: "#projects",
+    },
+    {
+      name: "Testimonials",
+      href: "#testimonials",
+    },
+    {
+      name: "Contact",
+      href: "#contact",
+    },
+  ];
+  
+  const bentoSocialLinks = [
+    {
+      name: "Facebook",
+      href: "https://www.facebook.com/",
+      icon: "/images/fb.svg",
+    },
+    {
+      name: "Instagram",
+      href: "https://www.facebook.com/",
+      icon: "/images/insta.svg",
+    },
+    {
+      name: "LinkedIn",
+      href: "https://www.facebook.com/",
+      icon: "/images/linkedin.svg",
+    },
+    {
+      name: "WhatsApp",
+      href: "https://www.facebook.com/",
+      icon: "/images/whatsapp.svg",
+    },
+  ];
+  
+  const iconsList = [
+    {
+      name: "html",
+      image: "/images/html.svg",
+    },
+    {
+      name: "css",
+      image: "/images/css.svg",
+    },
+    {
+      name: "javascript",
+      image: "/images/js.svg",
+    },
+    {
+      name: "react",
+      image: "/images/react.svg",
+    },
+    {
+      name: "typescript",
+      image: "/images/ts.svg",
+    },
+    {
+      name: "github",
+      image: "/images/github.svg",
+    },
+    {
+      name: "gsap",
+      image: "/images/gsap.svg",
+    },
+    {
+      name: "threejs",
+      image: "/images/threejs.svg",
+    },
+    {
+      name: "figma",
+      image: "/images/figma.svg",
+    },
+    {
+      name: "aws",
+      image: "/images/aws.svg",
+    },
+  ];
+  
+  const slides = [
+    {
+      id: 1,
+      title: "Sofi",
+      img: "/images/p1.png",
+    },
+    {
+      id: 2,
+      title: "Jasmina",
+      img: "/images/p2.png",
+    },
+    {
+      id: 3,
+      title: "d.tampe",
+      img: "/images/p3.png",
+    },
+    {
+      id: 4,
+      title: "Blimp.gr",
+      img: "/images/p4.png",
+    },
+    {
+      id: 5,
+      title: "Hawk Style Design",
+      img: "/images/p5.png",
+    },
+    {
+      id: 6,
+      title: "Lewis",
+      img: "/images/p6.png",
+    },
+    {
+      id: 7,
+      title: "Sofi",
+      img: "/images/p1.png",
+    },
+  ];
+  
+  const testimonials = [
+    {
+      name: "John Miller",
+      pos: "Founder of ModernEdge Solutions",
+      review:
+        "David Jhon turned our vision into a stunning, functional platform that our customers love. Their creativity and technical expertise truly set them apart.",
+      imgPath: "/images/client1.png",
+    },
+    {
+      name: "Emily Carter",
+      pos: "UX Designer at PixelWorks Studio",
+      review:
+        "David Jhon consistently brings fresh ideas and innovative solutions. Their passion for creativity and attention to detail elevate every project.",
+      imgPath: "/images/client2.png",
+    },
+    {
+      name: "Sarah Lopez",
+      pos: "Entrepreneur and Small Business Owner",
+      review:
+        "Exceeded my expectations with a unique and beautifully designed product that works flawlessly. Their creative touch is outstanding.",
+      imgPath: "/images/client3.png",
+    },
+    {
+      name: "David Chen",
+      pos: "Project Manager at CreativeSphere Agency",
+      review:
+        "Blends technical skills with bold creativity to deliver exceptional results. They push boundaries and elevate every project they work on.",
+      imgPath: "/images/client4.png",
+    },
+  ];
+  
+  const footerIconsList = [
+    {
+      name: "Facebook",
+      href: "https://www.facebook.com/",
+      icon: "/images/b-fb.svg",
+    },
+    {
+      name: "Instagram",
+      href: "https://www.facebook.com/",
+      icon: "/images/b-insta.svg",
+    },
+    {
+      name: "LinkedIn",
+      href: "https://www.facebook.com/",
+      icon: "/images/b-linked.svg",
+    },
+    {
+      name: "WhatsApp",
+      href: "https://www.facebook.com/",
+      icon: "/images/b-whatsapp.svg",
+    },
+  ];
+  
+  export {
+    navItems,
+    bentoSocialLinks,
+    iconsList,
+    slides,
+    testimonials,
+    footerIconsList,
+  };
---
+++ src/sections/Hero.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <section id='home' className='border border-red-500 h-dvh relative text-white-50 px-5 md:p-0'>
+        <div className='w-full h-full flex-center'>
+            <div className="container relative w-full h-full">
+                <div className="md:mt-40 mt-20"></div>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default Hero
